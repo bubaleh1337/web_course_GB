fc417497d3748268c14b41426d87593de0317fa2
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-06-14 19:04:27 +0500

    finding solution for button 'change text'

--- sem2/assets/scripts/script2.js
+++ sem2/assets/scripts/script2.js
@@ -1,9 +1,34 @@
-let button = document.getElementById('bt21');
+// let count = 0;
+
+// function changeText() {
+//   let element = document.getElementById('head1');
+//   element.textContent = 'ЗАКАЗЫВАЙТЕ ТОЛЬКО У НАС 😎';
+
+//   if (count % 2 == 0) {
+//     element.append(element.textContent);
+//   }
+//   else {
+//     element.remove(element.textContent);
+//   }
+//   count++;
+// }
+
+// let button = document.getElementById('bt21');
+// button.addEventListener('click', changeText);
+
+let count1 = 0;
 
 function changeText() {
   let element = document.getElementById('head1');
-  element.textContent = 'До встречи';
-  console.log(element);
+  element.textContent = 'ЗАКАЗЫВАЙТЕ ТОЛЬКО У НАС 😎';
+  
+  if (count1 % 2 == 0) {
+    element.append(element.textContent);
+  }
+  else {
+    element.remove(element.textContent);
+  }
+  count1++;
 }
-
-button.addEventListener('click', changeText);
+let btn21 = document.getElementById('btn21');
+btn21.addEventListener('click', changeText);
